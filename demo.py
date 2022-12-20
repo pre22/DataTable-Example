ffcc7cb1357ae070af5a04f3119d81d77789104f
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-12-20 06:48:41 +0100

    db practice

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, relationship
 # Create Engine
 engine = create_engine('sqlite+pysqlite:///db.sqlite3', echo=False)
 
@@ -15,17 +15,38 @@ class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
+
     username = Column(String(50))
     password = Column(String(50))
 
+    def __repr__(self):
+        return f"{self.username}"
+
+
+class SessionDB(Base):
+    __tablename__ = 'user_session'
+
+    id = Column(Integer, primary_key=True)
+
+    user_id = Column(Integer, ForeignKey(User.id, ondelete="CASCADE"))
+    user = relationship(User, back_populates="username")
+
+    session_item = Column(String(300))
+    session_value= Column(String(300))
+
+    def __str__(self):
+        return f"{user} | {session_item}"
+
+# User.addresses = relationship("SessionDB", order_by=SessionDB.id, back_populates="user")
+
 # Migrate
-# Base.metadata.create_all(engine)
+Base.metadata.create_all(engine)
 
 # Add Data 
 # user_one = User(username="pre.chika.22@gmail.com", password="user1234")
 # user_two = User(username="pre.chika.22@gmail.com", password="user1234")
 # user_three = User(username="pre.chika.22@gmail.com", password="user1234")
-# # session.add(user_one)
+# session.add(user_one)
 # session.add_all([user_two, user_three])
 # session.commit()
 
@@ -34,6 +55,6 @@ class User(Base):
 # data = session.query(User).filter(User.id == 1).one
 # print(data)
 
-data = session.query(User).filter(User.username=="pre.chika.22@gmail.com").first()
-data.password = "user2424"
-session.commit()
+# data = session.query(User).filter(User.username=="pre.chika.22@gmail.com").first()
+# data.password = "user2424"
+# session.commit()
